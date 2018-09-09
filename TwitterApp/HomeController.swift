f3b86cffb56119816a1f1966d5539d6b7861a281
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-09 13:23:42 -0700

    created new storyboard. Set color to green

---
+++ TwitterApp/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  TwitterApp
+//
+//  Created by Welinkton on 9/9/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .green
+    }
+}
