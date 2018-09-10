5bb22724ea3ccd0582f45cc942827c34dfcc49d4
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-09 17:40:36 -0700

    created WordCell class and updated HomeController class

--- TwitterApp/HomeController.swift
+++ TwitterApp/HomeController.swift
@@ -9,9 +9,51 @@
 import Foundation
 import UIKit
 
-class HomeController: UICollectionViewController {
+class WordCell: UICollectionViewCell {
+    
+    // This gets called when we need to dequeue a cell
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+   
+    // Named the reused cell
+    let cellId = "cellId"
+    
+    // Changed the background color to green.  Registered the Reusue Cell
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = .green
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: "cellId")
+        
+    }
+
+    // Indicated how many number of rows for the view
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+
+    //
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        // cells are transparent when initialized.  Gave them a color
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 }
