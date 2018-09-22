0395c0bad94828e21019053db45aa807f7edf2af
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-22 03:20:20 -0700

    Cell color configuration

--- TwitterApp/HomeController.swift
+++ TwitterApp/HomeController.swift
@@ -34,25 +34,30 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     // Changed the background color to green.  Registered the Reusue Cell
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .green
+        collectionView?.backgroundColor = .white
         collectionView?.register(WordCell.self, forCellWithReuseIdentifier: "cellId")
         
     }
 
     // Indicated how many number of rows for the view
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return 4
     }
 
-    //
+    // grab a cell and reuse it.
+    // also grab a cell and confiure it. the text, etc.
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         
         // cells are transparent when initialized.  Gave them a color
+        // Cell is being changed to blue immediately after being set to yellow.
         cell.backgroundColor = .blue
+       
+        
         return cell
     }
     
+    // configures the size of and width of the cell
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: 50)
     }
